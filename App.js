1e0f282357b7c698acbf83ac5adcdb1acf906dc0
humayun-sys <60429396+humayun-sys@users.noreply.github.com>
2020-12-30 12:01:57 +0530

    this is my app js commit

---
+++ App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Service from './pages/Service';
+import Single from './pages/Single';
+import Gallery from './pages/Gallery';
+import Contact from './pages/Contact';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+    <Router>
+
+      <Header/>
+         <Switch>
+            <Route exact path = '/' component = {Home}/>
+            <Route path = '/about' component = {About}/>
+            <Route path = '/service' component = {Service}/>
+            <Route path = '/single' component = {Single}/>
+            <Route path = '/gallery' component = {Gallery}/>
+            <Route path = '/contact' component = {Contact}/>
+         </Switch>
+      
+      <Footer/>
+
+    </Router>
+    
+    </>
+  );
+}
+
+export default App;
